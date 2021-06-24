4c4acab998c34b8a1f38f848be76ad36ac676129
GitHub Action <action@github.com>
2021-06-24 11:51:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/thetbw/springjumptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/thetbw/springjumptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.thetbw.springjumptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thetbw.springjumptemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/thetbw/springjumptemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/thetbw/springjumptemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.thetbw.springjumptemplate.services
 
+import com.github.thetbw.springjumptemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
